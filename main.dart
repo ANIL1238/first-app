f1d31e5b7e55e2c9545f33c9261d5f5608149f83
Anil kumar sahoo <77315978+ANIL1238@users.noreply.github.com>
2021-12-08 22:39:52 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+void main() {
+  runApp(Center(
+    child: Text(
+      "hello u", textDirection: TextDirection.ltr,
+      style: TextStyle(fontSize: 50.0))))
+  ;
+  //Text("welcome ,te")
+}
